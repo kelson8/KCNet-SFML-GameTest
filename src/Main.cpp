4171d363091d4439e08801ec2dfd4e4c9a04cee4
kelson8 <kelson@kelsoncraft.net>
2025-01-24 13:27:43 -0500

    Add basic window, add closing the window with escape and changing its color to grey.

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,12 +9,65 @@
 #include <SFML/Network.hpp>
 
 
-// https://www.youtube.com/watch?v=C06eGdy7C6k&list=PL6xSOsbVA1eb_QqMTTcql_3PdOiE928up&index=4
+// https://www.youtube.com/watch?v=kxb0GvBNOGU&list=PL6xSOsbVA1eb_QqMTTcql_3PdOiE928up&index=5
 // I got the linker and include files fixed for this in Debug and Release
 
 int main()
 {
 	Defines defines = Defines();
+
+	// About in console
 	std::cout << "Welcome to: " << defines.windowTitle << std::endl;
 	std::cout << "Version: " << defines.programVersion << std::endl;
+
+	//Window
+	sf::RenderWindow window(sf::VideoMode(defines.screenHeight, defines.screenWidth), defines.windowTitle, sf::Style::Titlebar | sf::Style::Close);
+	sf::Event event;
+
+	// Game loop
+	while (window.isOpen())
+	{
+		// Event polling
+		// While we are getting events from the window, save them in here
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+
+			// Close the window
+			case sf::Event::Closed:
+				window.close();
+				break;
+
+			// Keys pressed
+			case sf::Event::KeyPressed:
+				// Close the window using escape.
+				if (event.key.code == sf::Keyboard::Escape)
+				{
+					window.close();
+				}
+				break;
+			}
+		}
+
+		// Update
+
+
+		// Render
+		// Use this to generate colors:
+		// https://www.rapidtables.com/web/color/RGB_Color.html
+		// Light blue
+		// window.clear(sf::Color(102, 178, 255, 255)); // Clear old frame
+		// Gray
+		window.clear(sf::Color(64, 64, 64, 255)); // Clear old frame
+
+		// Draw your game
+
+
+		window.display(); // Tell app that window is done drawing.
+	}
+
+	// End of application
+
+
 }
